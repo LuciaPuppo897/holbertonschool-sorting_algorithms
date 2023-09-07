2e9682e03d05cd1427853150e35767d40d54c413
LuciaPuppo897 <alerome897@gmail.com>
2023-09-07 10:16:13 -0700

    Fixed betty and add 0-O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,19 +9,19 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    int i, j, tmp;
+	int i, j, tmp;
 
-    for (i = size - 1; i >= 0; i--)
-    {
-        for (j = 0; j < i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                tmp = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = size - 1; i >= 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
