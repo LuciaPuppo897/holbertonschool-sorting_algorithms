2a9813c49267b12a219fbe93e15806ef856a4fcc
gastonperego <gastonperego@gmail.com>
2023-09-13 00:06:19 +0000

    fix: big bugs

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,14 +12,14 @@ void quick_sort(int *array, size_t size)
     int max = size, min = 0;
     
 
-    recursive_quick(array, min, max);
+    recursive_quick(array, min, max, size);
 }
 
 
 /**
 * 
 */
-void recursive_quick (int *array, int min, int max)
+void recursive_quick (int *array, int min, int max, size_t size)
 {
     int i = min, j = min, piv = array[max - 1];
 
@@ -30,18 +30,23 @@ void recursive_quick (int *array, int min, int max)
     {
         if (array[j] <= piv)
         {
+            /*printf("---------> %d\n", piv);
             printf("Antes de swap : %d, %d\n", array[i], array[j]);
             printf("Antes de swap : %d, %d\n", i, j);
-            swap(&array[i], &array[j]);
             printf("Después de swap : %d, %d\n", array[i], array[j]);
-            printf("Después de swap : %d, %d\n", i, j);
+            printf("%d\n" , max);
+            printf("Después de swap : %d, %d\n", i, j);*/
+            swap(&array[i], &array[j]);
             i++;
-            print_array(array, max);
+            if (array[j] == piv)
+                break;
         }
     }
-
-    recursive_quick(array, i + 1, max);
-    recursive_quick(array, min ,i -1);
+    print_array(array, size);
+    if (array[j] != array[min])
+        recursive_quick(array, min ,i - 1, size);
+    if (array[j] != piv)
+        recursive_quick(array, i, max, size);
 }
 
 void swap(int *a, int *b)
