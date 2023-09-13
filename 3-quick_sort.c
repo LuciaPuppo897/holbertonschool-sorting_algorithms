215a7de7736b3519225c0a42335c1fa69eaa5e37
LuciaPuppo897 <alerome897@gmail.com>
2023-09-13 12:48:23 -0700

    fixed errors

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,49 +1,43 @@
 #include "sort.h"
 /**
-* quick_sort - sorts an array using quick sort
-*
-* @array: attay to sort
-* @size: size of the array
-*
-* Return: void
-*/
-void quick_sort(int *array, size_t size)
-{
-	int max = size, min = 0;
-
-	if (!array)
-		return;
-
-	recursive_quick(array, min, max, size);
-}
-
-
-/**
-*
+* recursive_quick - the recursive function to call
+*@array: the array to sort
+*@min: the first value on the array
+*@max: the last value of the array
+*@size: the size of the array
 */
 void recursive_quick(int *array, int min, int max, size_t size)
 {
-	int i = min, j = min, piv = array[max - 1];
 
-	if (!array[min] || !array[max - 1])
-		return;
-
-	for (; j < max; j++)
+	if (min < max)
 	{
-		if (array[j] <= piv)
-		{
-			swap(&array[i], &array[j], i, j, array, size);
-			i++;
-			if (array[j] == piv)
-				break;
-		}
-	}
-	if (array[j] != array[min])
-		recursive_quick(array, min, i - 1, size);
-	if (array[j] != piv)
-		recursive_quick(array, i, max, size);
-}
+		int i = min, j, piv = array[max];
 
+		if (!array[min] || !array[max - 1])
+			return;
+
+		for (j = min; j < max; j++)
+		{
+			if (array[j] <= piv)
+			{
+				swap(&array[i], &array[j], i, j, array, size);
+				i++;
+			}
+		}
+			swap(&array[i], &array[max], i, j, array, size);
+		recursive_quick(array, min, i - 1, size);
+		recursive_quick(array, i + 1, max, size);
+	}
+}
+/**
+*swap - swaps two values
+*@a: the first value to swap
+*@b: the second value to swap
+*@i: iterative var
+*@j: iterative var
+*@array: the array to swap
+*@size: the size of the array
+*/
 void swap(int *a, int *b, int i, int j, int *array, size_t size)
 {
 	int tmp;
@@ -55,7 +49,24 @@ void swap(int *a, int *b, int i, int j, int *array, size_t size)
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
-	
-	if (i != j || array[i] != array[j])
+
+	if (i != j && array[i] != array[j])
 		print_array(array, size);
+}
+/**
+* quick_sort - sorts an array using quick sort
+*
+* @array: attay to sort
+* @size: size of the array
+*
+* Return: void
+*/
+void quick_sort(int *array, size_t size)
+{
+	int max = size - 1, min = 0;
+
+	if (!array)
+		return;
+
+	recursive_quick(array, min, max, size);
 }
